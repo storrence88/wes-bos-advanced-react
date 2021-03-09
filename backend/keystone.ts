6b01f4f88730405a6ec55e09d178ab1b17f8d0a2
Steven Torrence <storrence88@gmail.com>
2021-03-09 15:50:46 -0600

    Update keystone.ts config file

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,10 +1,31 @@
 import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
 
-const databaseURL =
-  process.env.DATABASE_URL || 'mongo://localhost/keystone-sick-fits-tutorial';
+const databaseURL = process.env.DATABASE_URL || 'mongo://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 365, // How long should they stay signed in
   secret: process.env.COOKIE_SECRET
 };
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true
+    }
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+  }),
+  ui: {
+    // TODO: Change this for roles
+    isAccessAllowed: () => true
+  }
+  // TODO: Add session values here
+});
