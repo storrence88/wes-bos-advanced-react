fdfd1155143125dadbdc28d01cf077942c453b86
Steven Torrence <storrence88@gmail.com>
2021-03-23 11:07:05 -0500

    Update allProducts query to take in variables for skip and first and apply logic for that in the Products component

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,10 +2,11 @@ import { useQuery } from '@apollo/client';
 import styled from 'styled-components';
 import gql from 'graphql-tag';
 import Product from './Product';
+import { perPage } from '../config';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(skip: $skip, first: $first) {
       id
       name
       description
@@ -28,8 +29,13 @@ const ProductList = styled.div`
   grid-gap: 60px;
 `;
 
-const Products = () => {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+const Products = ({ page }) => {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage
+    }
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
