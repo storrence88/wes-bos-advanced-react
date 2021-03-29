6f14e47a6c26d1c9195d2766c38bdbda729302a1
Steven Torrence <storrence88@gmail.com>
2021-03-29 16:51:14 -0500

    Create password reset email

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import 'dotenv/config';
 import { insertSeedData } from './seed-data';
+import { sendPasswordResetEmail } from './lib/mail';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -24,7 +25,7 @@ const { withAuth } = createAuth({
   },
   passwordResetLink: {
     async sendToken(args) {
-      console.log(args);
+      await sendPasswordResetEmail(args.token, args.identity);
     }
   }
 });
