a2f40ea5aa3ac837175fe483fc63268c1d17a5c6
Steven Torrence <storrence88@gmail.com>
2021-03-30 10:58:18 -0500

    Fix .gitignore

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -7,7 +7,15 @@ export const User = list({
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
-    password: password()
+    password: password(),
+    cart: relationship({
+      ref: 'CartItem.user',
+      many: true,
+      ui: {
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' }
+      }
+    })
     // TODO: Add roles, cart, and orders
   }
 });
