23e80a47fe88afb9ba5905f60cc1845840ea6f7d
Steven Torrence <storrence88@gmail.com>
2021-03-30 15:19:24 -0500

    Use React context, create provider and consumer, and create open and close functionality for cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,9 +1,11 @@
 import CartStyles from './styles/CartStyles';
 import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
 import { useUser } from './User';
 import styled from 'styled-components';
 import formatMoney from '../lib/formatMoney';
 import calcTotalPrice from '../lib/calcTotalPrice';
+import { useCart } from '../lib/cartState';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
@@ -22,7 +24,7 @@ const CartItemStyles = styled.li`
 const CartItem = ({ cartItem }) => {
   const { product } = cartItem;
   if (!product) return null;
-  console.log(product);
+
   return (
     <CartItemStyles>
       <img width='100' src={product.photo.image.publicUrlTransformed} alt={product.name} />
@@ -41,12 +43,14 @@ const CartItem = ({ cartItem }) => {
 
 const Cart = () => {
   const current_user = useUser();
+  const { cartOpen, closeCart } = useCart();
   if (!current_user) return null;
-  console.log({ current_user });
+
   return (
-    <CartStyles open>
+    <CartStyles open={cartOpen}>
       <header>
         <Supreme>{current_user.name}'s Cart</Supreme>
+        <CloseButton onClick={closeCart}>&times;</CloseButton>
       </header>
       <ul>
         {current_user.cart.map((cartItem) => (
