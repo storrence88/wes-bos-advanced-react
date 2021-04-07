11302b58cc7fb93bceee3b35f1ff04b8e7846f47
Steven Torrence <storrence88@gmail.com>
2021-04-07 11:54:36 -0500

    Creating order and order item from mutation

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -72,5 +72,33 @@ export default async function checkout(
   console.log(charge);
 
   // 5. Convert the cartItems to orderItems
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } }
+    };
+
+    return orderItem;
+  });
+
   // 6. Create the Order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } }
+    }
+  });
+
+  // 7. Clean up any old cart items
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds
+  });
+
+  return order;
 }
