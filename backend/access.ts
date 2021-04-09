5c6b37701efa06622161bae9ce01c9edc4fa675a
Steven Torrence <storrence88@gmail.com>
2021-04-09 17:04:43 -0500

    User and field based permission

--- backend/access.ts
+++ backend/access.ts
@@ -70,5 +70,15 @@ export const rules = {
 
     // They should only see available products (based on the status field)
     return { status: 'AVAILABLE' };
+  },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    // Otherwise they may only update themselves!
+    return { id: session.itemId };
   }
 };
